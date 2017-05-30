1a09917cb712cee75fdc5fcf05cab040e28e2996
kpratik2015 <frek1546@gmail.com>
2017-05-31 01:35:22 +0530

    Pipes

--- angular2-templates/app/app.component.ts
+++ angular2-templates/app/app.component.ts
@@ -25,13 +25,34 @@ import { Component } from '@angular/core';
         {{ i + 1 }} - {{ course }}
       </li>
     </ul>
+    <br/>
+    {{ movie.title | uppercase | lowercase }}
+    <br/>
+    {{ movie.rating | number:'2.2-2' }}
+    <br/>
+    {{ movie.raters | number }}
+    <br/>
+    {{ movie.ticketCost | currency:'INR':true:'3.2-2' }}
+    <br/>
+    {{ movie.releaseDate | date:'MMM yyyy'}}
+    <br/>
+    {{ movie | json }}
   `  //template is by default part of HTML 5. It stays hidden until activated. If images or scripts are put in template then those also won't be loaded until template is activated.
       /*
       Note: In Angular v4 <template> has been deprecated in favour of <ng-template> and will be removed in v5. In Angular v2.x releases <template> is still valid.
       */
+
 })
 export class AppComponent { 
   courses = ['Course1','Course2','Course3'];
+
+  movie = {
+    title: "Inception",
+    rating: 9.9345,
+    raters: 59348,
+    ticketCost: 220.50,
+    releaseDate: new Date(2016, 2, 3)
+  }
 
   viewMode = 'map';
 }
